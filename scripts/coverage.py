887a4d84a321949fefc6e49c489aa58db1382d69
LChooaa <LChooaa@outlook.com>
2025-03-31 17:40:48 +0800

    feat: add cover name file generation and parsing function

--- scripts/coverage.py
+++ scripts/coverage.py
@@ -7,7 +7,83 @@ import argparse
 
 from runtools import NOOP_HOME, BMCFUZZ_HOME
 
-MAX_COVER_POINTS = 38253
+MAX_COVER_POINTS = 0
+
+def generage_cover_name_file():
+    firrtl_cover_file = os.path.join(NOOP_HOME, "build", "generated-src", "firrtl-cover.cpp")
+    cover_name_file = os.path.join(BMCFUZZ_HOME, "scripts", "cover_name.dat")
+
+    cover_name_begin_pattern = re.compile(r"_NAMES\[\]")
+    cover_name_end_pattern = re.compile(r"};")
+
+    point2name = {}
+
+    with open(firrtl_cover_file, "r") as f:
+        lines = f.readlines()
+        index = 0
+        cover_name_begin = False
+        for line in lines:
+            if cover_name_begin and cover_name_end_pattern.search(line):
+                break
+            if cover_name_begin:
+                match = re.search(r"\"(.*?)\"", line)
+                if match:
+                    name = match.group(1)
+                    point2name[index] = name
+                    index += 1
+            if cover_name_begin_pattern.search(line):
+                cover_name_begin = True
+    
+    with open(cover_name_file, "w") as f:
+        f.write("Index,Name\n")
+        for index, name in point2name.items():
+            f.write(f"{index},{name}\n")
+
+def parse_cover_name_file():
+    global MAX_COVER_POINTS
+    cover_name_file = os.path.join(BMCFUZZ_HOME, "scripts", "cover_name.dat")
+    point2name = {}
+    with open(cover_name_file, "r") as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            index = int(row['Index'])
+            name = row['Name']
+            point2name[index] = name
+        MAX_COVER_POINTS = len(point2name)
+    return point2name
+
+def parse_cover_name(src_file):
+    point2name = parse_cover_name_file()
+    
+    output_dir = os.path.dirname(src_file)
+    covered_file = os.path.join(output_dir, "covered.csv")
+    uncovered_file = os.path.join(output_dir, "uncovered.csv")
+    
+    covered_points = {}
+    uncovered_points = {}
+    with open(src_file, "r") as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            index = int(row['Index'])
+            covered = int(row['Covered'])
+            if covered:
+                covered_points[index] = point2name[index]
+            else:
+                uncovered_points[index] = point2name[index]
+    
+    with open(covered_file, "w", newline='', encoding='utf-8') as file:
+        field_name = ['Index', 'Name']
+        csv_writer = csv.DictWriter(file, fieldnames=field_name)
+        csv_writer.writeheader()
+        for index, name in covered_points.items():
+            csv_writer.writerow({'Index': index, 'Name': name})
+    
+    with open(uncovered_file, "w", newline='', encoding='utf-8') as file:
+        field_name = ['Index', 'Name']
+        csv_writer = csv.DictWriter(file, fieldnames=field_name)
+        csv_writer.writeheader()
+        for index, name in uncovered_points.items():
+            csv_writer.writerow({'Index': index, 'Name': name})
 
 def parse_uncovered_log(file_path, output_path):
     with open(file_path, "r") as f:
@@ -15,6 +91,8 @@ def parse_uncovered_log(file_path, output_path):
     
     # cover_pattern = re.compile(r"hexbin/cover_(\d+).bin")
     cover_pattern = re.compile(r"(\d+):\w+")
+
+    point2name = parse_cover_name_file()
     
     cover_points = [1 for _ in range(MAX_COVER_POINTS)]
     for line in lines:
@@ -23,12 +101,12 @@ def parse_uncovered_log(file_path, output_path):
             cover_points[int(match.group(1))] = 0
     
     with open(output_path, "w", newline='', encoding='utf-8') as file:
-        field_name = ['Index', 'Covered']
+        field_name = ['Index', 'Covered', 'Name']
         csv_writer = csv.DictWriter(file, fieldnames=field_name)
         csv_writer.writeheader()
         
         for i in range(MAX_COVER_POINTS):
-            csv_writer.writerow({'Index': i, 'Covered': cover_points[i]})
+            csv_writer.writerow({'Index': i, 'Covered': cover_points[i], 'Name': point2name[i]})
     
     # with open(file_path, "w") as f:
     #     f.write("")
@@ -40,6 +118,8 @@ def parse_covered_log(file_path, output_path):
     with open(file_path, "r") as f:
         lines = f.readlines()
     
+    point2name = parse_cover_name_file()
+    
     cover_points = [0 for _ in range(MAX_COVER_POINTS)]
     for line in lines:
         if uncover_case_pattern.search(line):
@@ -49,18 +129,20 @@ def parse_covered_log(file_path, output_path):
             cover_points[int(match.group(1))] = 1
     
     with open(output_path, "w", newline='', encoding='utf-8') as file:
-        field_name = ['Index', 'Covered']
+        field_name = ['Index', 'Covered', 'Name']
         csv_writer = csv.DictWriter(file, fieldnames=field_name)
         csv_writer.writeheader()
         
         for i in range(MAX_COVER_POINTS):
-            csv_writer.writerow({'Index': i, 'Covered': cover_points[i]})
+            csv_writer.writerow({'Index': i, 'Covered': cover_points[i], 'Name': point2name[i]})
     
     # with open(file_path, "w") as f:
     #     f.write("")
     
 
 def diff_covered_points(file1, file2):
+    point2name = parse_cover_name_file()
+    
     file1_covered = [0 for _ in range(MAX_COVER_POINTS)]
     with open(file1, mode='r', newline='', encoding='utf-8') as file:
         csv_reader = csv.DictReader(file)
@@ -74,14 +156,6 @@ def diff_covered_points(file1, file2):
         for row in csv_reader:
             if int(row['Covered']) == 1:
                 file2_covered[int(row['Index'])] = 1
-    
-    point2name = {}
-    cover_name_file = os.path.join(BMCFUZZ_HOME, "scripts", "cover_name.dat")
-    with open(cover_name_file, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            index, name = line.strip().split(",")
-            point2name[int(index)] = name
     
     file1_add = []
     file2_add = []
@@ -118,7 +192,10 @@ if __name__ == "__main__":
     os.chdir(NOOP_HOME)
 
     parser = argparse.ArgumentParser()
+
+    parser.add_argument("--generate-cover-name", '-g', action='store_true', help="Generate cover name file")
     
+    parser.add_argument("--parse-name", '-pn', action='store_true', help="Parse cover name file")
     parser.add_argument("--parse-covered", '-pc', action='store_true', help="Parse covered log")
     parser.add_argument("--parse-uncovered", '-pu', action='store_true', help="Parse uncovered log")
     parser.add_argument("--diff", '-d', action='store_true', help="Diff hexbin log")
@@ -128,6 +205,11 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    if args.generate_cover_name:
+        generage_cover_name_file()
+
+    if args.parse_name:
+        parse_cover_name(args.file1)
     if args.parse_covered:
         parse_covered_log(args.file1, args.file2)
     if args.parse_uncovered:

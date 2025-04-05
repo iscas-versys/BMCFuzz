4cb333c29278b6c28fe68131a2d3a84d208f56ff
LChooaa <LChooaa@outlook.com>
2025-04-05 09:21:04 +0800

    refactor: update rtl

--- Formal/demo/rocket_toggle/GEN_w32_toggle.v
+++ Formal/demo/rocket_toggle/GEN_w32_toggle.v
@@ -5,7 +5,7 @@ module GEN_w32_toggle(
   input reset,
   input [32 - 1: 0] valid
 );
-  parameter COVER_TOTAL = 9715;
+  parameter COVER_TOTAL = 8065;
   parameter COVER_INDEX;
 `ifndef SYNTHESIS
 `ifdef DIFFTEST

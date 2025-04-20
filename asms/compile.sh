fb6c63013564c6c7e8016b7572ad566bec21a507
Chooaa <LChooaa@outlook.com>
2025-04-20 21:56:52 +0800

    feat: update all annotation in asms dir

--- asms/compile.sh
+++ asms/compile.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-# 遍历当前目录下所有以.S结尾的文件
+# Traverse all files ending with .S in the current directory
 for asm_file in *.S; do
-    # 去掉文件扩展名
+    # Remove the file extension
     base_name="${asm_file%.S}"
     
-    # 编译生成目标文件
+    # Assemble to generate object file
     riscv64-linux-gnu-as -o "${base_name}.o" "$asm_file"
     
-    # 链接生成ELF文件
+    # Link to generate ELF file
     # riscv64-linux-gnu-ld -o "${base_name}.elf" "${base_name}.o"
     riscv64-linux-gnu-ld -T linker.ld -o "${base_name}.elf" "${base_name}.o"
     # riscv64-linux-gnu-ld -T linker.ld -o program.elf program.o
-    # 生成BIN文件
+
+    # Generate binary file
     riscv64-linux-gnu-objcopy -O binary "${base_name}.elf" "${base_name}.bin"
     
-    # 生成反汇编文件
+    # Generate disassembly file
     riscv64-linux-gnu-objdump -D "${base_name}.elf" > "${base_name}.dump"
     
     echo "Processed $asm_file: Generated ${base_name}.o, ${base_name}.elf, ${base_name}.bin, ${base_name}.dump"
